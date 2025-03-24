c77d566897cb229feed66cd5511f250fb2ff3b37
AdUnivGit <dn.univ@proton.me>
2025-03-24 08:08:33 +0000

    mise en ligne

---
+++ terre01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+indication_valeur1 = ord("a")  # pour savoir quelle 
+indication_valeur2 = ord("z")  # valeur numérique = quelle lettre
+
+# print(f"{indication_valeur1} et {indication_valeur2}") 
+# Résultat : a = 97 et z = 122 
+ 
+for i in range(97, 123):
+    print(chr(i), end="")
+print("\n")
+
+
+
+#
